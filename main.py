5eed17946375c2fce9460b83ecd51ab61aeaa8f3
pragadajayasree <134396518+pragadajayasree@users.noreply.github.com>
2024-05-21 19:18:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,53 +30,62 @@ resources = {
     "coffee": 100,
 }
 money = 0
+
+
 def report():
     print("water:", resources["water"], "ml")
     print("milk:", resources["milk"], "ml")
     print("coffee:", resources["coffee"], "g")
     print("money: $", money)
-def coins():
-     print ("please insert coins")
-     x = int(input("How many quarters? "))
-     y = int(input("How many dines? "))
-     z = int(input("How many nickles? "))
-     s = int(input("How many pennies? "))
-     amount = x*0.25+y*0.10+z*0.05+s*0.01
-     return amount
 
-def check(amount,p):
-    if amount < p["cost"]:
+
+def coins():
+    print("please insert coins")
+    x = int(input("How many quarters? "))
+    y = int(input("How many dines? "))
+    z = int(input("How many nickles? "))
+    s = int(input("How many pennies? "))
+    amount = x * 0.25 + y * 0.10 + z * 0.05 + s * 0.01
+    return amount
+
+
+def check(amount, m):
+    if amount < m["cost"]:
         return 0
     else:
         return 1
-def change(p,amount,m):
-    if amount !=p["cost"]:
-        print(f"Here is ${round(amount-p["cost"],2)} in change")
+
+
+def change(s, amount, m):
+    if amount != s["cost"]:
+        print(f"Here is ${round(amount - s["cost"], 2)} in change")
     print(f"here is your {m} ☕ enjoy!")
 
-def deduct(p):
-    global money
-    for i in p["ingredients"]:
-        resources[i]-=p["ingredients"][i]
-    money+=p["cost"]
 
-def satisfy(p):
-    for i in p:
-        if resources[i]<p[i]:
+def deduct(s):
+    global money
+    for i in s["ingredients"]:
+        resources[i] -= s["ingredients"][i]
+    money += s["cost"]
+
+
+def satisfy(s):
+    for i in s:
+        if resources[i] < s[i]:
             print(f"sorry! there is no enough {i}")
             return 0
     return 1
 
-def select(p,m):
-    q=satisfy(p["ingredients"])
-    if(q):
+
+def select(s, m):
+    q = satisfy(s["ingredients"])
+    if q:
         amount = coins()
-        if check(amount,p) == 0:
+        if check(amount, s) == 0:
             print("sorry!,that's not enough money. money refunded.")
         else:
-            change(p,amount,m)
-            deduct(p)
-
+            change(s, amount, m)
+            deduct(s)
 
 
 a = True
@@ -85,4 +94,4 @@ while a:
     if p == "report":
         report()
     else:
-       select(MENU[p],p)
+        select(MENU[p], p)
